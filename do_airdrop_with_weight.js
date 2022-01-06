d8f3f105021530da1bff078082211571a911bd0e
NOCTLJRNE <baoan1602@gmail.com>
2022-01-06 18:03:32 +0700

    epoch 825 compensation

--- do_airdrop_with_weight.js
+++ do_airdrop_with_weight.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const numFormat = require('./utils/numberFormat');
 
 // Open file and put addresses into a list to iterate over
-fName_str = '819_JEWEL_airdrop'
+fName_str = 'testaddresses'
 var fName = `${fName_str}.csv`
 var ouput_fileName = `${fName_str}.log`
 const lineByLine = require('n-readlines')
